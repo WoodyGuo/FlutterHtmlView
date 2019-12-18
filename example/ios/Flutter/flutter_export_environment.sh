8112529dec4c5186ec97ff985f60400d0581004c
Len <w.liu@holaverse.com>
2019-12-18 11:26:13 +0800

    插件更新

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/len/gitwork/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/len/gitwork/Dict/deps/FlutterHtmlView/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/len/gitwork/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
